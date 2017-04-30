932c3ba2b1e270a6383b830bf3a887c81a0ae417
Keyboard-l <keyboard-w@outlook.com>
2017-04-30 16:54:40 +0800

    add some page

--- js/main.js
+++ js/main.js
@@ -78,6 +78,77 @@ function switchOff(){  // 游戏停止
             document.getElementById(id).className = "off";
         }
     }
+
+}
+function reload()
+{
+    var i, j, id;
+    status = 0;
+    livePro = [0, 0, 1, 1, 0, 0, 0, 0, 0];
+    bornPro = [0, 0, 0, 1, 0, 0, 0, 0, 0];
+    for(i = 0;i < 9;i++){
+        if(livePro[i])
+            document.getElementById("live"+i.toString()).className = "checkbox on";
+        else
+            document.getElementById("live"+i.toString()).className = "checkbox off";
+        if(bornPro[i])
+            document.getElementById("born"+i.toString()).className = "checkbox on";
+        else
+            document.getElementById("born"+i.toString()).className = "checkbox off";
+    }
+    for(i = 0;i < 50;i++){
+        for(j = 0;j < 50;j++){
+            id = "on"+mytoString(i+1)+mytoString(j+1);
+            document.getElementById(id).className = "off";
+        }
+    }
+    document.getElementById("on0826").className = "on";
+    document.getElementById("on0924").className = "on";
+    document.getElementById("on0926").className = "on";
+    document.getElementById("on1014").className = "on";
+    document.getElementById("on1015").className = "on";
+    document.getElementById("on1022").className = "on";
+    document.getElementById("on1023").className = "on";
+    document.getElementById("on1113").className = "on";
+    document.getElementById("on1117").className = "on";
+    document.getElementById("on1122").className = "on";
+    document.getElementById("on1123").className = "on";
+    document.getElementById("on1136").className = "on";
+    document.getElementById("on1137").className = "on";
+    document.getElementById("on1212").className = "on";
+    document.getElementById("on1218").className = "on";
+    document.getElementById("on1222").className = "on";
+    document.getElementById("on1223").className = "on";
+    document.getElementById("on1236").className = "on";
+    document.getElementById("on1237").className = "on";
+    document.getElementById("on1302").className = "on";
+    document.getElementById("on1303").className = "on";
+    document.getElementById("on1312").className = "on";
+    document.getElementById("on1316").className = "on";
+    document.getElementById("on1318").className = "on";
+    document.getElementById("on1319").className = "on";
+    document.getElementById("on1324").className = "on";
+    document.getElementById("on1326").className = "on";
+    document.getElementById("on1402").className = "on";
+    document.getElementById("on1403").className = "on";
+    document.getElementById("on1412").className = "on";
+    document.getElementById("on1418").className = "on";
+    document.getElementById("on1426").className = "on";
+    document.getElementById("on1513").className = "on";
+    document.getElementById("on1517").className = "on";
+    document.getElementById("on1614").className = "on";
+    document.getElementById("on1615").className = "on";
+}
+function hover(list, line, n)
+{
+    if(n==1){
+        document.getElementById(list).className = "localon";
+        document.getElementById(line).className = "localon";
+    }
+    else{
+        document.getElementById(list).className = "localoff";
+        document.getElementById(line).className = "localoff";
+    }
 }
 
 function turn(id) {  // 转换cell的状态
